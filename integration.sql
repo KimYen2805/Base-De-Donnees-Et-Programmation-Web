ec6b1f6a95ac568429bd4fec8ca5d63c63030ed4
LE KIM YEN p2107434 <kim-yen.le@etu.univ-lyon1.fr>
2023-11-20 20:20:49 +0000

    Replace integration.sql

--- integration.sql
+++ integration.sql
@@ -1,27 +1,44 @@
-INSERT INTO Commune(codePostal, nom, coordonnées, codeINSEE_3, adresseMairie, codeINSEE_2)
+INSERT INTO Région (codeINSEE_1, nom)
 SELECT DISTINCT
-CONVERT(code_postal, SIGNED INTEGER), 
-nom_commune_complet, 
-CONCAT(latitude, ',', longitude) AS coordonnées, 
-CONVERT(code_commune_INSEE, SIGNED INTEGER) AS codeINSEE_3, 
-NULL AS adresseMairie,
-CONVERT(code_departement, SIGNED INTEGER) AS codeINSEE_2
-FROM 
-dataset.Communes
-WHERE 
-nom_region = "Auvergne-Rhône-Alpes" ;
+    CONVERT(code_region, SIGNED INTEGER) AS codeINSEE_1,
+    nom_region AS nom
+FROM
+    dataset.Communes
+WHERE
+    nom_region = "Auvergne-Rhône-Alpes"
+ON DUPLICATE KEY UPDATE nom = VALUES(nom);
 
-INSERT IGNORE INTO Région(codeINSEE_1,nom) 
+INSERT INTO Département (codeINSEE_2, nom, codeINSEE_1)
 SELECT DISTINCT
-CONVERT(code_region, SIGNED INTEGER) AS codeINSEE_1,
-nom_region AS nom
-FROM dataset.Communes
-WHERE nom_region = "Auvergne-Rhône-Alpes";
+    CONVERT(code_departement, SIGNED INTEGER) AS codeINSEE_2,
+    nom_departement AS nom,
+    code_region AS codeINSEE_1
+FROM
+    dataset.Communes
+WHERE
+    nom_region = "Auvergne-Rhône-Alpes"
+ON DUPLICATE KEY UPDATE nom = VALUES(nom), codeINSEE_1 = VALUES(codeINSEE_1);
 
-INSERT IGNORE INTO Département(codeINSEE_2, nom, codeINSEE_1)
+
+INSERT INTO Commune (codePostal, nom, coordonnées, codeINSEE_3, adresseMairie, codeINSEE_2)
 SELECT DISTINCT
-CONVERT(code_departement, SIGNED INTEGER), 
-nom_departement, 
-code_region AS codeINSEE_1
-FROM dataset.Communes
-WHERE nom_region = "Auvergne-Rhône-Alpes";
+    CONVERT(code_postal, SIGNED INTEGER) AS codePostal,
+    nom_commune_complet AS nom,
+    CONCAT(latitude, ',', longitude) AS coordonnées,
+    CONVERT(code_commune_INSEE, SIGNED INTEGER) AS codeINSEE_3,
+    NULL AS adresseMairie,
+    CONVERT(code_departement, SIGNED INTEGER) AS codeINSEE_2
+FROM
+    dataset.Communes AS d
+WHERE
+    nom_region = "Auvergne-Rhône-Alpes"
+    AND NOT EXISTS (
+        SELECT 1
+        FROM Commune AS c
+        WHERE c.codePostal = CONVERT(d.code_postal, SIGNED INTEGER)
+           OR c.codeINSEE_3 = CONVERT(d.code_commune_INSEE, SIGNED INTEGER)
+    );
+
+
+
+
