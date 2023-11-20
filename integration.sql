342b43eecd0802847e0bb1fe9a3545724449254c
LE KIM YEN p2107434 <kim-yen.le@etu.univ-lyon1.fr>
2023-11-20 14:20:07 +0000

    Replace integration.sql

--- integration.sql
+++ integration.sql
@@ -1,18 +1,16 @@
-INSERT IGNORE  INTO Commune (idCommune, codePostal, nom, coordonnées, codeINSEE_3, adresseMairie, codeINSEE_2)
-SELECT 
-code_commune AS idCommune,
+
+INSERT  INTO Commune (codePostal, nom, coordonnées, codeINSEE_3, adresseMairie, codeINSEE_2)
+SELECT DISTINCT
 CONVERT( code_postal, SIGNED INTEGER), 
 nom_commune_complet, 
 CONCAT(latitude, ',', longitude) AS coordonnées, 
 CONVERT(code_commune_INSEE, SIGNED INTEGER) AS codeINSEE_3, 
-'',
+NULL AS adresseMairie,
 CONVERT(code_departement, SIGNED INTEGER) AS codeINSEE_2
 FROM 
 dataset.Communes
 WHERE 
-nom_region = "Auvergne-Rhône-Alpes";
-
-
+nom_region = "Auvergne-Rhône-Alpes" ;
 
 INSERT IGNORE INTO Région(codeINSEE_1,nom) 
 SELECT DISTINCT
@@ -25,6 +23,6 @@ INSERT IGNORE INTO Département(codeINSEE_2, nom, codeINSEE_1)
 SELECT DISTINCT
 CONVERT(code_departement, SIGNED INTEGER), 
 nom_departement, 
-''
+code_region AS codeINSEE_1
 FROM dataset.Communes
 WHERE nom_region = "Auvergne-Rhône-Alpes";
