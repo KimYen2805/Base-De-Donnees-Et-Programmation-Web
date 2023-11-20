d6652362efbd31bdad45d7e3fc60123b68d4f40d
LE KIM YEN p2107434 <kim-yen.le@etu.univ-lyon1.fr>
2023-11-20 15:27:39 +0000

    Replace vueAccueil.php

--- vues/vueAccueil.php
+++ vues/vueAccueil.php
@@ -1,7 +1,24 @@
-<h2><?= $message ?></h2> <!--Informations sur la BDD -->
+<div class="container">
+ <div class= "pic">
+    <img 
+    src="img/img1.jpg" />
+</div>
+<div id="paragDescription" class ="content">
+<h1>Bienvenue sur le site CommuNet</h1>
+    <p>Ce site propose des outils efficaces pour la gestion et le traitement des données de manière pratique. Découvrez les fonctionnalités suivantes :</p>
 
-<p id="paragDescription">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla, ante eu commodo malesuada, arcu erat pellentesque dui, sed scelerisque ex velit ornare tellus. Donec tincidunt pulvinar consectetur. Maecenas ac justo lorem. Praesent rhoncus mi eget nibh egestas tincidunt. Etiam scelerisque ex justo, eu commodo massa tincidunt ut. Aenean eu justo in magna porta luctus non nec ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec accumsan nibh. Aenean pulvinar odio sit amet lobortis auctor. In efficitur lacus in mauris sagittis semper. Quisque dignissim risus nec tincidunt auctor.
-    <br>
-    Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla tristique, tellus a lobortis efficitur, leo ex elementum neque, non hendrerit lacus lorem a quam. Vestibulum ut vehicula libero. Donec sagittis vulputate nibh a placerat. Etiam elementum lobortis risus, id accumsan ipsum sodales a. Ut posuere sit amet dui in congue. Nam est dui, rutrum et dolor in, convallis pellentesque massa. Nunc in rutrum sapien.
-</p>
+    <ul>
+        <li><strong>Afficher les statistiques:</strong>
+         Cette section vous permet d'afficher des informations sur la stabilité des données. Gérez et contrôlez les variations pour assurer la stabilité des données.</li>
+        <li><strong>Intégrer les données:</strong>
+         Cette fonction se concentre sur l'intégration de données provenant de différentes sources. Assurez la cohérence et la synchronisation entre les ensembles de données.</li>
+        <li><strong>Ajouter une Service:</strong>
+         Ici, vous pouvez ajouter de nouveaux services, étendant les capacités de traitement et d'analyse des données. Créez des services flexibles répondant rapidement à vos besoins.</li>
+        <li><strong>Générer une liste de périodes d'essai:</strong> 
+        Cette fonction vous aide à créer une liste de périodes d'essai, essentielle pour évaluer les performances et la stabilité du système.</li>
+    </ul>
+
+    <p>Le site a été spécialement conçu pour les étudiants en BDW, offrant une expérience pratique et efficace dans la gestion et le traitement des données. Explorez et profitez des utilités que nous offrons !</p>
+</div>
+</div>
+<h2><?= $message ?></h2> <!--Informations sur la BDD -->
