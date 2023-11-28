75247b92488912149517137072829263fda485e7
LE KIM YEN p2107434 <kim-yen.le@etu.univ-lyon1.fr>
2023-11-28 00:42:37 +0000

    Replace controleurGenerer.php

--- controleurs/controleurGenerer.php
+++ controleurs/controleurGenerer.php
@@ -1,27 +1,11 @@
 <?php
+
 $connexion = getConnexionBD(); // connexion à la BD
 
 if (isset($_POST['boutonGenerer'])) {
     $departement = $_POST['departement'];
     $mois_max = $_POST['mois_max'];
     $kilometres = $_POST['kilometres'];
-
-    $trialResults = genererPeriodesEssai($connexion, $departement, $mois_max, $kilometres);
-
-    echo "Résultats des périodes d'essai";
-
-    if (empty($trialResults)) {
-        echo "Aucune période d'essai générée. Raison : [Ajoutez un message de débogage ici]";
-    } else {
-        foreach ($trialResults as $res) {
-            echo "<p>Commune: {$res['commune']} <br>
-            - Durée: {$res['duree']} mois <br>
-            - Services: " . implode(', ', $res['services']) . "</p>";
-        }
-    }
-    
-        
-        
-    }
-
-?>
+    genererPeriodesEssai($connexion,$departement, $mois_max, $kilometres);
+}
+ ?>
